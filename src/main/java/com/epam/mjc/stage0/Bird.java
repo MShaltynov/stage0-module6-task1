ddc1a694ae0e0f4a11f0923e493bced3566ee173
Mikhail <shaltynovm@gmail.com>
2023-07-12 21:09:51 +0530

    initial commit

--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -1,5 +1,15 @@
 package com.epam.mjc.stage0;
 
-public class Bird {
+public class Bird extends Animal {
 
+    public Bird() {
+        super.setColor("blue");
+        super.setNumberOfPaws(2);
+        super.setHasFur(false);
+    }
+
+    @Override
+    public String getDescription() {
+        return "This animal is mostly blue. It has 2 paws and no fur. Moreover, it has 2 wings and can fly.";
+    }
 }
